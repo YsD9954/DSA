5a2c3b78901f85ab9408a20181495b5f79bc4351
YsD9954 <yashdoke1234@gmail.com>
2025-06-28 15:01:39 +0530

    Initial commit

---
+++ src/III_NON_LINEAR_DATA_STRUCTURE/II_TREES/II_BINARY_SEARCH_TREE/YOO_XXXVI_CHECK_DEADEND_PRESENT_OR_NOT.java
@@ -0,0 +1,7 @@
+package III_NON_LINEAR_DATA_STRUCTURE.II_TREES.II_BINARY_SEARCH_TREE;
+
+public class YOO_XXXVI_CHECK_DEADEND_PRESENT_OR_NOT {
+    public static void main(String[] args) {
+
+    }
+}
