ebb342d42643892bfb5e33e542d474cc375ae0b0
YsD9954 <yashdoke1234@gmail.com>
2025-07-02 12:24:51 +0530

    Initial commit

---
+++ src/III_NON_LINEAR_DATA_STRUCTURE/III_GRAPHS/TOPOLOGICAL_SORTING.java
@@ -0,0 +1,24 @@
+package III_NON_LINEAR_DATA_STRUCTURE.III_GRAPHS;
+
+public class TOPOLOGICAL_SORTING {
+    public static void main(String[] args) {
+
+    }
+}
+
+
+
+//NOTE => TOPOLOGICAL SORTING...
+//1. ACTION 1 -> ACTION 2-> ACTION 3 -|__\  ACTION 5 .......
+//                      |-> ACTION 4 -|  /
+
+//
+//        1
+//        |
+//        2
+//      /   \
+//     3     4
+//      \   /
+//        5
+
+//SO TOPLOGICAL SORTING WILL BE 1->2->3->4->5 OR 1->2->4->3->5
